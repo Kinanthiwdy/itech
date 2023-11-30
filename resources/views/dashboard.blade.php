33b5dcad8de3c4bb3c6af1df9a310050c74c64e1
RainSmooch <olishmuhammad@gmail.com>
2023-11-30 15:14:59 +0700

    a

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -36,51 +36,27 @@
                 </div>
             </div>
 
-            <div class="row justify-content-center styleLayanan">
+            <div class="row justify-content-around styleLayanan">
                 
-                    <div class="col-md-4 stylecolLayanan imgicon" style="padding: 10px">
+                    <div class="col-md-4 stylecolLayanan imgicon" >
                         <img src="bukuIcon.png" alt="Icon Buku" width="50" height="50" style="padding-bottom: 10px">
                         <h6 class="teksicon">Materi Belajar</h6>
-                        <a href="/materibelajar" style="text-decoration: none; color: blue;" class="teksicon">Read More <svg
-                                xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
-                                class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd"
-                                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5" />
-                                <path fill-rule="evenodd"
-                                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z" />
-                            </svg></a>
+                        <a href="/materibelajar" style="text-decoration: none; color: blue;" class="teksicon">Read More</a>
                     </div>
-                    <div class="col-md-4 stylecolLayanan imgicon" style="padding: 10px">
+                    <div class="col-md-4 stylecolLayanan imgicon" >
                         <img src="dataIcon.png" alt="Icon Buku" width="50" height="50" style="padding-bottom: 10px">
                         <h6 class="teksicon">Statistika Hasil Belajar</h6>
-                        <a href="/statistika" style="text-decoration: none; color: blue" class="teksicon">Read More <svg
-                                xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
-                                class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd"
-                                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5" />
-                                <path fill-rule="evenodd"
-                                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z" />
-                            </svg></a>
+                        <a href="/statistika" style="text-decoration: none; color: blue" class="teksicon">Read More</a>
                     </div>
-                    <div class="col-md-4 imgicon" style="padding: 10px">
+                    <div class="col-md-4 imgicon" >
                         <img src="chatIcon.png" alt="Icon Buku" width="50" height="50" style="padding-bottom: 10px">
                         <h6 class="teksicon">Chat Admin</h6>
-                        <a href="https://linktr.ee/AdminITech" style="text-decoration: none; color: blue" class="teksicon">Read More <svg
-                                xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
-                                class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd"
-                                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5" />
-                                <path fill-rule="evenodd"
-                                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z" />
-                            </svg></a>
+                        <a href="https://linktr.ee/AdminITech" style="text-decoration: none; color: blue" class="teksicon">Read More</a>
                     </div>
-                
             </div>
-
-        </div>
-        </div>
         </div>
     </section>
+
     <section id="tentangKami">
         <div class="container text-center">
             <div class="row">
